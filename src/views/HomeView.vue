19166070292e1dfad44f01ba0b1efb20d9b96c79
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 21:01:37 +0100

    Edit main banner images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,26 @@ import Navbar from "@/components/headerComponents/Navbar.vue"
 <style scoped>
 /* mainbanner */
 .mainbanner {
-  background: url("@/assets/gallery/bg-forest-cover.png");
+  position: relative;
+  background: url("/assets/gallery/bg-forest-cover.png");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.mainbanner .bannertitle {
+  font-size: 6.5rem;
+  line-height: 6.5rem;
+  letter-spacing: 0.8rem;
+}
+
+.mainbanner .cover-banner {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  top: 0px;
+  left: 0px;
+  position: absolute;
 }
 </style>
 
@@ -16,12 +35,13 @@ import Navbar from "@/components/headerComponents/Navbar.vue"
 
 
     <!-- Main-banner -->
-    <div class="mainbanner">
-      <div class="container col-lg-10 col-md-11 col-sm-12">
-        <div class="bannercontent">
-          <h1 class="display-2 fw-bold bannertitle w-100 d-flex justify-content-center">FRAGRANCE</h1>
+    <div class="mainbanner cover-container d-flex w-100 vh-100 p-3 mx-auto flex-column py-5">
+      <div class="container col-lg-10 col-md-11 col-sm-12 py-5">
+        <div class="bannercontent py-5 d-flex flex-column align-items-center mt-3">
+          <h1 class="fw-semibold bannertitle mt-5">FRAGRANCE</h1>
         </div>
       </div>
+      <div class="cover-banner"></div>
     </div>
     <!-- .../Main-banner -->
 
