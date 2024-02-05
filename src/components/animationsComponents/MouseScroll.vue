f5cd381ce46f4a379eb3b26108ff0434d8b79eea
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 23:38:01 +0100

    Add mouse borders to MouseScroll Component

--- src/components/animationsComponents/MouseScroll.vue
+++ src/components/animationsComponents/MouseScroll.vue
@@ -1,4 +1,23 @@
-<style scoped></style>
+<style scoped>
+.mouse-bx .mouse {
+    width: 35px;
+    height: 60px;
+    border-radius: 50px !important;
+    border: 2px solid #ffffff8f;
+    display: block;
+    cursor: pointer !important;
+}
 
-<template></template>
+.mouse-bx .mouse:hover {
+    border: 2px solid #ffffff;
+}
+</style>
+
+<template>
+    <div class="mouse-bx d-block">
+        <span class="mouse">
+            <span class="mouse-indicator"></span>
+        </span>
+    </div>
+</template>
 
