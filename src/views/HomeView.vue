ba548f5ebdbb8a054780c3cd33ab93fc77de5d03
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 20:43:10 +0100

    minor changes in home.vue...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,9 +8,9 @@ import Navbar from "@/components/headerComponents/Navbar.vue"
   <Navbar />
 
   <main>
-    <div class="container  col-lg-10 col-md-11 col-sm-12">
 
-    </div>
+
+   
 
 
   </main>
