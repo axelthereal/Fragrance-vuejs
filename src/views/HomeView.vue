c7722e8bf2f155f834fe04eb2f8ff35301222e21
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 20:52:19 +0100

    add mainbanner cover img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,12 @@
 import Navbar from "@/components/headerComponents/Navbar.vue"
 </script>
 
-<style scoped></style>
+<style scoped>
+/* mainbanner */
+.mainbanner {
+  background: url("@/assets/gallery/bg-forest-cover.png");
+}
+</style>
 
 <template>
   <Navbar />
@@ -11,7 +16,13 @@ import Navbar from "@/components/headerComponents/Navbar.vue"
 
 
     <!-- Main-banner -->
-
+    <div class="mainbanner">
+      <div class="container col-lg-10 col-md-11 col-sm-12">
+        <div class="bannercontent">
+          <h1 class="display-2 fw-bold bannertitle w-100 d-flex justify-content-center">FRAGRANCE</h1>
+        </div>
+      </div>
+    </div>
     <!-- .../Main-banner -->
 
 
