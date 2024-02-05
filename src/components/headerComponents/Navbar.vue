f04bbbd9574b2953e3b600fa3a472aba106ba97e
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 15:53:40 +0100

    modified homeview

--- src/components/headerComponents/Navbar.vue
+++ src/components/headerComponents/Navbar.vue
@@ -3,7 +3,7 @@
 <template>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Third navbar example">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Expand at sm</a>
+            <a class="navbar-brand" href="#">Expand at sm 0</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03"
                 aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
