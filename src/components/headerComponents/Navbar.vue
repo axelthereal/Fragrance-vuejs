9216b37f0aa5b01bbea2a0f53fcb0375f26c259f
axelthereal <mail.axeltokam@gmail.com>
2024-02-05 16:23:04 +0100

    Edit main navbar design

--- src/components/headerComponents/Navbar.vue
+++ src/components/headerComponents/Navbar.vue
@@ -10,22 +10,33 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarsExample03">
-                <ul class="navbar-nav me-auto mb-2 mb-sm-0 w-100 justify-content-end">
+                <ul class="navbar-nav mx-auto mb-2 mb-sm-0 justify-content-center gap-3">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Blog</a>
+                        <a class="nav-link" href="#">Collection</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mb-2 mb-sm-0 d-flex justify-content-end gap-2">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <span class="bi bi-search lead fw-normal"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            <span class="bi bi-handbag lead fw-normal"></span>
+                        </a>
                     </li>
                 </ul>
             </div>
