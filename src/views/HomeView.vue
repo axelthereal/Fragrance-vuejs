ab9758a90082e1bd78c2c88274a9db6d4444beec
axelthereal <mail.axeltokam@gmail.com>
2024-02-07 13:14:07 +0100

    modified page banner's img

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,6 +71,22 @@ import MouseScroll from "@/components/animationsComponents/MouseScroll.vue";
   width: 150px;
   height: auto;
 }
+
+
+
+/* page-banner */
+.page-banner .float-img-banner {
+  position: absolute;
+  width: 50%;
+}
+
+.page-banner .float-img-banner .image {
+  position: relative;
+}
+
+.page-banner .float-img-banner .image img {
+  position: absolute;
+}
 </style>
 
 <template>
@@ -113,24 +129,12 @@ import MouseScroll from "@/components/animationsComponents/MouseScroll.vue";
     </div>
     <!-- .../Brands-banner -->
 
+
     <!-- page-banner -->
     <div class="page-banner">
-      <div class="container col-xxl-8 px-4 py-5">
+      <div class="col-lg-12 px-4 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div class="col-10 col-sm-8 col-lg-6">
-            <img src="bootstrap-themes.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700"
-              height="500" loading="lazy">
-          </div>
-          <div class="col-lg-6">
-            <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-            <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
-              popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-              extensive prebuilt components, and powerful JavaScript plugins.</p>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-              <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
-            </div>
-          </div>
+  
         </div>
       </div>
     </div>
