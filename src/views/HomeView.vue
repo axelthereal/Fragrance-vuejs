519f9502964bf49f735e61b6c1883c727a8667de
axelthereal <mail.axeltokam@gmail.com>
2024-02-08 01:48:34 +0100

    edit main banner images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -79,11 +79,6 @@ import MouseScroll from "@/components/animationsComponents/MouseScroll.vue";
   position: relative;
   overflow: hidden;
 }
-
-.page-banner .float-img-banner {
-  position: absolute;
-  width: 50%;
-}
 </style>
 
 <template>
@@ -129,12 +124,20 @@ import MouseScroll from "@/components/animationsComponents/MouseScroll.vue";
 
     <!-- page-banner -->
     <div class="page-banner">
-      <div class="float-img-banner mx-auto bg-primary p-5">
-      </div>
       <div class="container col-lg-10 col-md-11 col-sm-12 py-5">
         <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
-
-
+          <div class="col-12 col-sm-11 col-lg-6">
+            <img src="/assets/gallery/tempres/profile_girl2.png" class="w-100 h-auto" alt="">
+          </div>
+          <div class="col-12 col-sm-11 col-lg-6">
+            <h1 class="display-5 fw-bold lh-1 mb-3">Responsive left-aligned hero with image</h1>
+            <p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+              popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
+              extensive prebuilt components, and powerful JavaScript plugins.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+              <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
